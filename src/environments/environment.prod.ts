7f3914ae99cbba365a2bd6f2f30ed38d1eb6f633
gjteamone <giljohn.ferolino@teamone.com.sg>
2020-11-18 13:01:56 +0800

    Update Prod apuURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL of production API
-  apiUrl: 'https://payment-service-backend.herokuapp.com/'
+  apiUrl: 'https://payment-service-backend.herokuapp.com'
 };
